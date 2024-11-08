2c7c5cb3f402e23ced9defb3529654c5d29085df
IzazDf <izazdf.16@gmail.com>
2024-11-08 16:37:58 +0700

    perubahan fungsi 2

--- fungsi.cpp
+++ fungsi.cpp
@@ -2,8 +2,14 @@
 #include <cmath>
 #include <string>
 #include <emscripten.h>
+#include "windows.h"
 
 using namespace std;
+
+void myprint(const wchar_t* str){
+        HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+        WriteConsoleW(h, str, wcslen(str), NULL, NULL);
+    }
 
 extern "C" {
     EMSCRIPTEN_KEEPALIVE
@@ -12,6 +18,9 @@ extern "C" {
         long double hasil_ohm = nilai;
         int x = awal;
         int y = akhir;
+
+        string satuan[]={"Tera ohm", "Giga ohm", "Mega ohm", "kilo ohm", "ohm", "mili ohm", "mikro ohm", "nano ohm", "pico ohm"};
+        wstring st [] = {L"TΩ", L"GΩ", L"MΩ", L"kΩ", L"Ω", L"mΩ", L"μΩ", L"nΩ", L"pΩ"};
 
         if (x > 5){
                 int z = x - 5;
@@ -31,8 +40,16 @@ extern "C" {
                 hasil = hasil_ohm;
             }
             
-            long double hasilmega = hasil_ohm / pow(1000, 2);
-            long double hasilmikro = hasil_ohm * pow(1000, 2);
+            long double hasil_mega = hasil_ohm / pow(1000, 2);
+            long double hasil_mikro = hasil_ohm * pow(1000, 2);
+
+            cout << nilai << " " << satuan[awal - 1] << " "; wcout << "("; myprint(st[awal-1].c_str()); wcout << ")"; cout << " = " << hasil << " " << satuan[akhir - 1] << " "; wcout << "("; myprint(st[akhir-1].c_str()); wcout << ")" << endl;
+            cout << nilai << " " << satuan[awal - 1] << " "; wcout << "("; myprint(st[awal-1].c_str()); wcout << ")"; cout << " = " << hasil_mega << " " << satuan[2] << " "; wcout << "("; myprint(st[2].c_str()); wcout << ")" << endl;
+            cout << nilai << " " << satuan[awal - 1] << " "; wcout << "("; myprint(st[awal-1].c_str()); wcout << ")"; cout << " = " << hasil_ohm << " " << satuan[4] << " "; wcout << "("; myprint(st[4].c_str()); wcout << ")" << endl;
+            cout << nilai << " " << satuan[awal - 1] << " "; wcout << "("; myprint(st[awal-1].c_str()); wcout << ")"; cout << " = " << hasil_mikro << " " << satuan[6] << " "; wcout << "("; myprint(st[6].c_str()); wcout << ")" << endl;
+
+
+            return hasil;
     }
 
     
